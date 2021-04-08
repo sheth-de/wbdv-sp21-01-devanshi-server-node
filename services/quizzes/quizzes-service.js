ed43bbcb7b5b1322f5628c7d21db28b7a6201c0d
DESKTOP-SO1RK4N\jannu <jannunzi@gmail.com>
2021-04-08 08:22:23 -0400

    added mongoose

--- services/quizzes/quizzes-service.js
+++ services/quizzes/quizzes-service.js
@@ -1,14 +1,21 @@
 
 const quizzes = require("./quizzes.json")
 
+const quizzesModel = require("../../db/quizzes/quizzes-model")
+
 const createQuiz = () => {}
 const findAllQuizzes = () => {
-    return quizzes
+    return quizzesModel.find()
+    // return quizzes
 }
 const findQuizById = (qid) => {
-    return quizzes.find((quiz) => {
-        return (quiz._id === qid)
-    })
+    return quizzesModel
+        .findById(qid)
+        .populate("questions")
+        .exec()
+    // return quizzes.find((quiz) => {
+    //     return (quiz._id === qid)
+    // })
 }
 const updateQuiz = () => {}
 const deleteQuiz = () => {}
